9e71f03ee2cbf31125681abddff4e25e3ddf4871
AP Java Period 5 <apjavaperiod5@ac1mwk0mvjc9h.78b505.nycboe.org>
2022-10-03 12:07:58 -0400

    added fav movie files

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @ OliviaLarmand
+ * @09/21/22
+ */
+public class FavoriteMovie 
+{
+   public static void main(String[] args) {
+    System.out.println("My favorite movie is Tangled. ");
+    System.out.println("Tangled is a disney movie about a girl named Rapunzel that was locked away in a tower since she was captured as a baby by an old woman. ");
+    System.out.println("Her magical long blonde hair has the power to provide eternal youth, and the old women who kidnapped her uses this power to keep herself young. ");
+    System.out.println("At the age of 18, Rapunzel becomes curious about the outside world, and when a prince uses her tower to hid she asks him to help her escape. ");
+    System.out.println("They explore together the things Rapunzel has missed out on, along they way they fall in love and she finds out the truth about who she is.");
+   }
+}
