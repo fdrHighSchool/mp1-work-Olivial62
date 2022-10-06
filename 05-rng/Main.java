38447ec23a1d3e5cd3e9abbc2e3f4c4687bb9a90
AP Java Period 5 <apjavaperiod5@ac1mwk0mvjc9h.78b505.nycboe.org>
2022-10-06 11:59:57 -0400

    added modes to rng

--- 05-rng/Main.java
+++ 05-rng/Main.java
@@ -3,16 +3,33 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
     Random random = new Random();
     int number = random.nextInt(100) + 1;
+    System.out.println("What mode do you you want? \nEasy\nMedium\nHard");
     int userGuess;
-    Scanner in = new Scanner(System.in);
-    System.out.print("Guess my number: ");
-    userGuess = in.nextInt();
-    System.out.println("Your guess was: " + userGuess);
-    System.out.println(number);
-    int offBy;
-    offBy= number-userGuess; 
-    System.out.println("you were off by: " + offBy);
-      }
-}
+    
+
+    System.out.print("Enter your guess: ");
+    int playerGuess = s.nextInt(); 
+
+        // if the player guessed incorrectly
+        if(playerGuess != number) {
+            // if the player guessed too low
+            if(playerGuess < number) {
+                System.out.println("You were off by " + (number - playerGuess));
+            } // end inner if statement
+            // if the player didn't guess too low (meaning, incorrect and too high!)
+            else {
+                System.out.println("You were off by " + (playerGuess - number));
+            } // end inner else statement
+        } // end outer if statement
+        // if the player didn't guess incorrectly (meaning, correct guess)
+        else {
+            System.out.println("Correct!!!");
+        } // end outer else statement
+    } // end main method
+} // end class
+    
+  
