97bc0761dd9a118546f1f3d08e572d895e0e5a09
AP Java Period 5 <apjavaperiod5@ac1mwk0mvjc9h.78b505.nycboe.org>
2022-10-20 14:08:19 -0400

    added level to username password

--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -14,6 +14,7 @@ public class UserName {
     String status = s.nextLine();
     System.out.println("What is your favorite number? ");
     String num = s.nextLine();
+    
     // test output
     // System.out.println("Hello " + initialize(firstName) + "." + initialize(lastName) + ".");
     if (status.equals("student")) {
@@ -24,6 +25,7 @@ public class UserName {
        System.out.println(initialize(firstName) + lastName + num + "@schools.nyc.gov");
     }//end of else statement
     s.close();
+    generatePassword(9);
     
   } // end main method
 
@@ -39,8 +41,18 @@ public class UserName {
     return n.substring(0,1);
   }// end of String initialize
   public static String generatePassword(int length) {
-    String password;
-  }//end of generate password
+    String password = "";
+    String password1 = "";
+    for (int i=0; i <length; i++) {
+        char cap = (char)(int)(Math.random()*(90-65+1)+65);
+        char low = (char)(int)(Math.random()*(122-97+1)+97);
+        char special =(char)(int)(Math.random()*(47-33+1)+33);
+        password += cap;
+        password1 += low;
+        System.out.println(password);
+    }
+    return password;
+  } //end of generate password
   /*
    * Name: generatePassword
    * Purpose: send back a random String of numbers
@@ -48,7 +60,9 @@ public class UserName {
    * Return: a single character (String)
    */
    // generate a random number 
-   // 65-90 capital 
-   // 97-122 lowercase
-   
+   // 65-90 capital *
+   // 97-122 lowercase *
+   // 33-47 special symbols
+   // convert integer to a char
+   // add char to the password string
 } // end class
